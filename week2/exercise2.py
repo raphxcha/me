46b7b4f70ad8946f424d175322c1071b0d042c21
raphxcha <raphaelachallita@gmail.com>
2020-06-10 12:15:15 +1000

    commit

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -8,21 +8,20 @@ linter with no errors or warnings!
 
 import string
 
-def GetLetter(index);
+def GetLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet(index]
+    return alphabet
 
-
-def week2exersise2();
-    indices = [12: 2, 26, 7, 0, 12, 4, 17]
-    wordArray = map(getLetter, indices)
-    wordArray[0] = wordArray[0].upper()
-    wordArray{1} = wordArray[1].upper()
-    wordArray[3} = wordArray[3].upper{}
-    secret_word="".join(wordArray)
+def week2exersise2():
+    indices = [12, 2, 26, 7, 0, 12, 4, 17]
+    WordArray [] = map(GetLetter, indices)
+    (WordArray[0] = WordArray[0].upper())
+    (WordArray[1] = WordArray[1].upper())
+    (WordArray[3] = WordArray[3].upper())
+    secret_word ="".join(WordArray)
     print(secret_word)
     return secret_word
 
 
-if __name__ = = "__main__":
-    prin(week2exersise2())
+if __name__ == "__main__":
+    print(week2exersise2())
