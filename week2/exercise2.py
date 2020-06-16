353a9aaa83273d7128df97432ece25eb1ed82dc1
raphxcha <raphaelachallita@gmail.com>
2020-06-16 10:58:37 +1000

    commit

--- week2/exercise2.py
+++ week2/exercise2.py
@@ -10,14 +10,15 @@ import string
 
 def GetLetter(index):
     alphabet = string.ascii_lowercase + " "
-    return alphabet
+    return alphabet[index]
+
 
 def week2exersise2():
     indices = [12, 2, 26, 7, 0, 12, 4, 17]
-    WordArray = map(GetLetter, indices)
-    WordArray[0] = WordArray[0].upper()
-    WordArray[1] = WordArray[1].upper()
-    WordArray[3] = WordArray[3].upper()
+    wordArray = list(map(GetLetter, indices)
+    wordArray[0] = wordArray[0].upper()
+    wordArray[1] = wordArray[1].upper()
+    wordArray[3] = wordArray[3].upper()
     secret_word ="".join(WordArray)
     print(secret_word)
     return secret_word
