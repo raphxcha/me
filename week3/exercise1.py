d0043a03f8c369353a7906fcad3da493b05046a7
raphxcha <raphaelachallita@gmail.com>
2020-06-17 10:21:01 +1000

    commit

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,17 @@ def loop_ranger(start, stop=None, step=1):
     The look up the docs for range(), you can answer this with just the range 
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+    the_numbers = []
+    x = start
+    while x < stop:
+        print(x)
+        the_numbers.append(x)
+        x = x + step
+    return  the_numbers
+    
+
+
+
 
 
 def lone_ranger(start, stop, step):
