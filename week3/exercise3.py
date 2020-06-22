3b434437e36f1783d3170561c054e12a2aa57d7e
raphxcha <raphaelachallita@gmail.com>
2020-06-22 10:29:49 +1000

    commit

--- week3/exercise3.py
+++ week3/exercise3.py
@@ -26,25 +26,12 @@ def advancedGuessingGame():
     purpose if you can!
     """
     print("\nWelcome to the guessing game!")
-    print("A number between 0 and _ ?")
+    print("\nI'm thinking of a number between 1 and 100.")
+    print("Try to guess it in five tries or less")
     upperBound = input("Enter an upper bound: ")
     print("OK then, a number between 0 and {} ?".format(upperBound))
-    upperBound = int(upperBound)
-
-    actualNumber = random.randint(0, upperBound)
-
-    guessed = False
-
-    while not guessed:
-        guessedNumber = int(input("Guess a number: "))
-        print("You guessed {},".format(guessedNumber),)
-        if guessedNumber == actualNumber:
-            print("You got it!! It was {}".format(actualNumber))
-            guessed = True
-        elif guessedNumber < actualNumber:
-            print("Too small, try again :'(")
-        else:
-            print("Too big, try again :'(")
+    upperBound = int(upperBound) 
+    
 
     return "You got it!"
     # the tests are looking for the exact string "You got it!". Don't modify that!
--- week3/exercise4.py
+++ week3/exercise4.py
@@ -23,6 +23,23 @@ def binary_search(low, high, actual_number):
     Use the VS Code debugging tools a lot here. It'll make understanding 
     things much easier.
     """
+    if high >= low: 
+  
+        mid = (high + low) // 2
+  
+        # If element is present at the middle itself 
+        if arr[mid] == x: 
+            return mid 
+  
+        # If element is smaller than mid, then it can only 
+        # be present in left subarray 
+        elif arr[mid] > x: 
+            return binary_search(arr, low, mid - 1, x) 
+  
+        # Else the element can only be present in right subarray 
+        else: 
+            return binary_search(arr, mid + 1, high, x) 
+  
     tries = 0
     guess = 0
     return {"guess": guess, "tries": tries}
